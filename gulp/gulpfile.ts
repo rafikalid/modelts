2a98dc80108c49a5e22d53f1a448239915af0004
khalid <khalid.rfk@gmail.com>
2021-09-27 11:18:21 +0100

    support common js

--- gulp/gulpfile.ts
+++ gulp/gulpfile.ts
@@ -1,37 +1,28 @@
 import Gulp from 'gulp';
 
-import { typescriptCompile } from './typescript.js';
+import { compileEsNext, compileCommonjs } from './typescript.js';
 // import { compileTest } from './test-files.js';
 
 const { watch, series } = Gulp;
 
 const argv = process.argv;
-// const isProd = argv.includes('--prod');
+const isProd = argv.includes('--prod');
 const doWatch = argv.includes('--watch');
 
 /** Watch modified files */
 function watchCb(cb: Function) {
 	if (doWatch) {
-		watch('src/**/*.ts', typescriptCompile);
+		watch('src/**/*.ts', compileEsNext);
 		// watch('src/app/graphql/schema/**/*.gql', graphQlCompile)
 	}
 	cb();
 }
 
 var tasks: any[];
-// if(argv.includes('--test')){
-// 	tasks=[
-// 		compileTest
-// 	];
-// } else {
-tasks = [
-	typescriptCompile,
-	// parallel([
-	// 	typescriptCompile,
-	// 	graphQlCompile
-	// ]),
-	watchCb,
-];
-// }
+if (isProd) {
+	tasks = [compileEsNext, compileCommonjs, watchCb];
+} else {
+	tasks = [compileEsNext, watchCb];
+}
 
 export default series(tasks);
--- gulp/typescript.ts
+++ gulp/typescript.ts
@@ -4,37 +4,52 @@
 import Gulp from 'gulp';
 import GulpTypescript from 'gulp-typescript';
 import SrcMap from 'gulp-sourcemaps';
-import {createImportTransformer} from './typescript-transformer.js';
-import JSON5 from 'json5';
-import {readFileSync} from 'fs';
+import { Converter } from 'typescript-path-fix';
+import GulpRename from 'gulp-rename';
 
-const {src, dest, lastRun}= Gulp;
-const {parse}= JSON5;
-
+const { src, dest, lastRun } = Gulp;
 // import {transform} from 'ts-transform-import-path-rewrite'
 
-//Load config
-const tsConfig= parse(readFileSync('tsconfig.json', 'utf-8'));
-const importTransformer= createImportTransformer(tsConfig.compilerOptions);
+const isProd = process.argv.includes('--prod');
 
-const isProd= process.argv.includes('--prod');
+const tsPathFix = new Converter('tsconfig.json');
 
 const TsProject = GulpTypescript.createProject('tsconfig.json', {
 	removeComments: isProd,
 	pretty: !isProd,
-	getCustomTransformers: ()=>({
-		after: [
-			importTransformer
-		]
-	})
+	target: 'ESNext',
+	module: 'ESNext',
+});
+const TsProjectCommonjs = GulpTypescript.createProject('tsconfig.json', {
+	removeComments: isProd,
+	pretty: !isProd,
+	target: 'ES2015',
+	module: 'CommonJS',
 });
 
- // import babel from 'gulp-babel';
- 
-export function typescriptCompile(){
-	return src('src/**/*.ts', {nodir: true, since: lastRun(typescriptCompile)})
+/** Compile as EsNext */
+export function compileEsNext() {
+	return src('src/**/*.ts', {
+		nodir: true,
+		since: lastRun(compileEsNext),
+	})
 		.pipe(SrcMap.init())
 		.pipe(TsProject())
+		.pipe(tsPathFix.gulp('.mjs'))
+		.pipe(GulpRename({ extname: '.mjs' }))
 		.pipe(SrcMap.write('.'))
-		.pipe(dest('dist'));
+		.pipe(dest('dist/module'));
+}
+
+/** Compile as Commonjs */
+export function compileCommonjs() {
+	return src('src/**/*.ts', {
+		nodir: true,
+		since: lastRun(compileCommonjs),
+	})
+		.pipe(SrcMap.init())
+		.pipe(tsPathFix.gulp())
+		.pipe(TsProjectCommonjs())
+		.pipe(SrcMap.write('.'))
+		.pipe(dest('dist/commonjs'));
 }
