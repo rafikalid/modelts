13bfde4f9b2a513ea20aefeb5baecc593245c882
khalid RAFIK <khalid.rfk@gmail.com>
2021-10-13 18:34:35 +0100

    cc

--- src/model/index.ts
+++ src/model/index.ts
@@ -10,12 +10,12 @@ export class Model {
 	}
 
 	/** Compile files into Model */
-	static from(globFilesPath: string): Model {
+	static scan(...globFilesPath: string[]): Model {
 		throw new Error('Unexpected call. You forgot to run the compiler!');
 	}
 
 	/** Convert Model into graphql */
-	static toGraphQL(globFilesPath: string): GraphQLSchema {
+	static toGraphQL(...globFilesPath: string[]): GraphQLSchema {
 		throw new Error('Unexpected call. You forgot to run the compiler!');
 	}
 }
