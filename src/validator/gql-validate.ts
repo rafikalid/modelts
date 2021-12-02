3bcc9a31f1f38b7f2590963cb491641ce1cf213e
RAFIK Khalid <khalid.rfk@gmail.com>
2021-12-02 23:18:54 +0100

    validate

--- src/validator/gql-validate.ts
+++ src/validator/gql-validate.ts
@@ -1,6 +1,24 @@
+import { Kind } from "..";
 import { InputObject, InputList } from "./model";
 
 /** Validate GraphQL Inputs */
 export function pipeInputGQL(schema: InputObject | InputList, parent: any, args: any, ctx: any, info: any) {
+	var stack: (InputObject | InputList)[] = [schema];
+	while (true) {
+		let node = stack.pop();
+		if (node == null) break;
+		switch (node.kind) {
+			case Kind.INPUT_OBJECT: {
+				node.
+					break;
+			}
+			case Kind.INPUT_LIST: {
+				break;
+			}
+			default: {
+				let n: never = node;
+			}
+		}
+	}
 	return args;
 }
