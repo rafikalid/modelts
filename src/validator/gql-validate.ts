500fb651f0b6a953608cbf6bf14a8ed9c0e8f93e
RAFIK Khalid <khalid.rfk@gmail.com>
2021-12-23 01:54:58 +0100

    validation

--- src/validator/gql-validate.ts
+++ src/validator/gql-validate.ts
@@ -13,8 +13,8 @@ export interface ValidationResult {
  */
 export async function pipeInputGQL(schema: InputObject | InputList, parent: any, args: any, ctx: any, info: any): Promise<ValidationResult> {
 	var result: any[] = [];
-	/** Stack: schema, parent, value, [validation for parent], [validation values from child] */
-	var stack: any[] = [schema, parent, args, result, []];
+	/** Stack: schema, parent, value, field_index, [validation for parent], [validation values from child] */
+	var stack: any[] = [schema, parent, args, 0, result, []];
 	/** State: 0: down, 1: up */
 	var states: (0 | 1)[] = [1, 0];
 	var errors = [];
@@ -28,6 +28,7 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 				let ln = stack.length;
 				let resultsFromChildren = stack[--ln] as any[];
 				let resultsToParent = stack[--ln] as any[];
+				let fieldIndex = stack[--ln] as any;
 				let args = stack[--ln] as any;
 				let parent = stack[--ln] as any;
 				let node = stack[--ln] as InputObject | InputList;
@@ -40,17 +41,19 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 							else args = node.before(parent, args, ctx, info);
 						}
 						// Validate fields
-						// for (let i = 0, fields = node.fields, len = fields.length; i < len; ++i) {
-						for (let fields = node.fields, i = fields.length - 1; i >= 0; --i) {
+						for (let i = 0, fields = node.fields, len = fields.length; i < len; ++i) {
 							let field = fields[i];
 							let data = args[field.alias];
-							// Assert
-							if (field.assert != null) field.assert(data);
-							// Go through sub type
-							if (data != null && field.type != null) {
-								states.push(1, 0); // up, down
-								// Stack: schema, parent, value, [validation values from child]
-								stack.push(field.type, args, data, resultsFromChildren, []);
+							resultsFromChildren.push(data);
+							if (data != null) {
+								// Assert
+								if (field.assert != null) field.assert(data);
+								// Go through sub type
+								if (field.type != null) {
+									states.push(1, 0); // up, down
+									// Stack: schema, parent, value, [validation values from child]
+									stack.push(field.type, args, data, i, resultsFromChildren, []);
+								}
 							}
 						}
 						break;
@@ -58,11 +61,12 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 					case Kind.INPUT_LIST: {
 						let type = node.type;
 						if (type != null) {
-							// for (let i = 0, len = args.length; i < len; ++i) {
-							for (let i = args.length - 1; i >= 0; --i) {
+							for (let i = 0, len = args.length; i < len; ++i) {
+								let data = args[i];
+								resultsFromChildren.push(data);
 								states.push(1, 0); // up, down
 								// Stack: schema, parent, value, [validation values from child]
-								stack.push(type, args, args[i], resultsFromChildren, []);
+								stack.push(type, args, data, i, resultsFromChildren, []);
 							}
 						}
 						break;
@@ -75,12 +79,15 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 				//* UP
 				let resultsFromChildren = stack.pop() as any[];
 				let resultsToParent = stack.pop() as any[];
+				let fieldIndex = stack.pop() as any;
 				let args = stack.pop() as any;
 				let parent = stack.pop() as any;
 				let node = stack.pop() as InputObject | InputList;
 				// Do validation
 				switch (node.kind) {
 					case Kind.INPUT_OBJECT: {
+						console.log('=====>', node.name)
+						console.log('===x==>', node.fields)
 						// Validate fields
 						let data: Record<string, any> = {};
 						for (let i = 0, fields = node.fields, len = fields.length; i < len; ++i) {
@@ -90,6 +97,7 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 								if (field.pipeAsync) v = await field.pipe(args, v, ctx, info);
 								else v = field.pipe(args, v, ctx, info);
 							}
+							console.log(field.name, '>', v)
 							data[field.name] = v;
 						}
 						// Post validation
@@ -97,11 +105,11 @@ export async function pipeInputGQL(schema: InputObject | InputList, parent: any,
 							if (node.afterAsync) data = await node.after(parent, data, ctx, info);
 							else data = node.after(parent, data, ctx, info);
 						}
-						resultsToParent.push(data);
+						resultsToParent[fieldIndex] = data;
 						break;
 					}
 					case Kind.INPUT_LIST: {
-						resultsToParent.push(resultsFromChildren);
+						resultsToParent[fieldIndex] = resultsFromChildren;
 						break;
 					}
 					default: {
