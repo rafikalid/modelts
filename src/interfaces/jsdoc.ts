c817a8da38a221de0d3e82c47d9643e39e12d9f3
RAFIK Khalid <khalid.rfk@gmail.com>
2022-04-01 18:52:41 +0000

    refactor(jsDoc): annotations

--- src/interfaces/jsdoc.ts
+++ src/interfaces/jsdoc.ts
@@ -1,4 +1,33 @@
-/** JsDoc annotations */
-export class JsDocAnnotations {
+/** Jsdoc format */
+export interface JsDocInterface {
+	[k: string]: (arg: string) => jsDocAnnotationResult
+}
 
+/** JsDoc annotation result */
+export interface jsDocAnnotationResult {
+	/** Additional imports */
+	imports?: {
+		name: string
+
+		lib: string
+	}[],
+	/** Insert code before execution */
+	before?: string | string[]
+	/** Insert code after execution */
+	after?: string | string[]
+}
+
+/** JsDoc annotations */
+export class JsDocAnnotations implements JsDocInterface {
+	[k: string]: (arg: string) => jsDocAnnotationResult;
+
+	/** Parse assertions */
+	assert(arg: string) {
+		return {};
+	}
+
+	/** Pase default value */
+	default(arg: string) {
+		return {};
+	}
 }
